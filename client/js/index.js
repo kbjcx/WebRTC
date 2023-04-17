2b91271555facc5c86b941858ce3a3ce5f9bcd69
kbjcx <lu.kbjcx@gmail.com>
2023-04-17 21:07:20 +0800

    changes

--- client/js/index.js
+++ client/js/index.js
@@ -21,40 +21,275 @@ const SIGNAL_TYPE_OFFER = "offer";
 const SIGNAL_TYPE_ANSWER = "answer";
 const SIGNAL_TYPE_CANDIDATE = "candidate";
 
-const constraints = {
-    video: true,
-    audio: true
+let local_user_id = Math.random().toString(36).substring(2); // 本地用户ID
+let remote_user_id = -1; // 远端用户ID
+let room_id = 0;
+
+let local_video = document.querySelector("#local-video");
+let remote_video = document.querySelector("#remote-video");
+
+let local_stream = null;
+let remote_stream = null;
+let pc = null; // RTCPeerConnection
+
+let rtc_engine;
+
+function handle_ice_candidate(event) {
+    console.info("handle_ice_candidate");
+    if (event.candidate) {
+        let json_msg = {
+            "cmd": SIGNAL_TYPE_CANDIDATE,
+            "room_id": room_id,
+            "uid": local_user_id,
+            "remote_uid": remote_user_id,
+            "msg": JSON.stringify(event.candidate)
+        };
+        let msg = JSON.stringify(json_msg);
+        rtc_engine.send_message(msg);
+        console.info("handle ice candidate message: " + msg);
+    }
+    else {
+        console.warn("End of candidates");
+    }
 }
 
-// 处理打开摄像头成功
-function handle_success(stream) {
-    const video = document.querySelector("#local-video");
-    video.srcObject = stream;
+function handle_remote_stream_add(event) {
+    console.info("handle_remote_stream_add");
+    remote_stream = event.streams[0];
+    remote_video.srcObject = remote_stream;
+}
+function create_peer_connection() {
+    pc = new RTCPeerConnection(null);
+    pc.onicecandidate = handle_ice_candidate;
+    pc.ontrack = handle_remote_stream_add;
+    local_stream.getTracks().forEach(track => pc.addTrack(track, local_stream));
 }
 
-// 摄像头打开异常处理
-function handle_error(error) {
-    console.log("getUserMedia error" + error);
+function on_create_offer(session) {
+    pc.setLocalDescription(session)
+        .then(function () {
+            let json_msg = {
+                "cmd": SIGNAL_TYPE_OFFER,
+                "room_id": room_id,
+                "uid": local_user_id,
+                "remote_uid": remote_user_id,
+                "msg": JSON.stringify(session)
+            }
+            let msg = JSON.stringify(json_msg);
+            rtc_engine.send_message(msg);
+            console.info("send offer message: " + msg);
+        })
+        .catch(function (error) {
+            console.error("offer setLocalDescription error: " + error);
+        });
 }
 
-// function on_open_camera(e) {
-//     navigator.mediaDevices.getUserMedia(constraints)
-//         .then(handle_success)
-//         .catch(handle_error)
-// }
+function on_create_answer(session) {
+    pc.setLocalDescription(session)
+        .then(function () {
+            let json_msg = {
+                "cmd": SIGNAL_TYPE_ANSWER,
+                "room_id": room_id,
+                "uid": local_user_id,
+                "remote_uid": remote_user_id,
+                "msg": JSON.stringify(session)
+            }
+            let msg = JSON.stringify(json_msg);
+            rtc_engine.send_message(msg);
+            console.info("send answer message: " + msg);
+        })
+        .catch(function (error) {
+            console.error("offer setLocalDescription error: " + error);
+        });
+}
 
-window.onload = function () {
-    navigator.mediaDevices.getUserMedia(constraints)
-        .then(handle_success)
-        .catch(handle_error);
+function handle_create_offer_error(error) {
+    console.error("handle create offer error: " + error);
+}
+
+function handle_create_answer_error(error) {
+    console.error("handle create answer error: " + error);
+}
+
+let RTCEngine = function (wsurl) {
+    this.init(wsurl);
+    rtc_engine = this;
+    return this;
 };
 
-document.querySelector("#turnoff-video").addEventListener("click", function () {
-    document.querySelector('#local-video').srcObject = null;
-});
+RTCEngine.prototype.init = function (wsurl) {
+    // 设置websocket url
+    this.wsurl = wsurl;
+    // 存储websocket对象
+    this.signal = null;
+};
 
-document.querySelector("#show-video").addEventListener("click", function () {
-        navigator.mediaDevices.getUserMedia(constraints)
-            .then(handle_success)
-            .catch(handle_error)
-});
+RTCEngine.prototype.create_websocket = function () {
+    rtc_engine = this;
+    rtc_engine.signal = new WebSocket(this.wsurl);
+
+    rtc_engine.signal.onopen = function () {
+        rtc_engine.on_open();
+    };
+    rtc_engine.signal.onmessage = function (ev) {
+        rtc_engine.on_message(ev);
+    };
+    rtc_engine.signal.onerror = function (ev) {
+        rtc_engine.on_error(ev);
+    };
+    rtc_engine.signal.onclose = function (ev) {
+        rtc_engine.on_close(ev);
+    }
+
+};
+
+RTCEngine.prototype.on_open = function () {
+    console.log("websocket open");
+};
+
+RTCEngine.prototype.on_message = function (event) {
+    console.log("on_message: " + event.data);
+
+    let json_msg = JSON.parse(event.data);
+    switch (json_msg.cmd) {
+        case SIGNAL_TYPE_NEW_PEER:
+            handle_remote_new_peer(json_msg);
+            break;
+        case SIGNAL_TYPE_RESP_JOIN:
+            handle_remote_response_join(json_msg);
+            break;
+        case SIGNAL_TYPE_PEER_LEAVE:
+            handle_remote_peer_leave(json_msg);
+            break;
+        case SIGNAL_TYPE_OFFER:
+            handle_remote_offer(json_msg);
+            break;
+        case SIGNAL_TYPE_ANSWER:
+            handle_remote_answer(json_msg);
+            break;
+        case SIGNAL_TYPE_CANDIDATE:
+            handle_remote_candidate(json_msg);
+            break;
+    }
+};
+
+RTCEngine.prototype.on_error = function (event) {
+    console.log("on_error: " + event.data);
+};
+
+RTCEngine.prototype.on_close = function (event) {
+    console.log("on_close -> code: " + event.code + ", reason: " + EventTarget.reason);
+}
+
+RTCEngine.prototype.send_message = function (message) {
+    this.signal.send(message);
+};
+
+function handle_remote_new_peer(message) {
+    console.info("handle remote new peer uid: " + message.remote_uid);
+    remote_user_id = message.remote_uid;
+    do_offer();
+}
+
+function handle_remote_response_join(message) {
+    console.info("handle remote new peer uid: " + message.remote_uid);
+    remote_user_id = message.remote_uid;
+}
+
+function handle_remote_peer_leave(message) {
+    remote_video.srcObject = null;
+    alert("对方退出了房间");
+}
+
+function handle_remote_offer(message) {
+    console.info("handle remote offer");
+    if (pc == null) {
+        create_peer_connection();
+    }
+    let description = JSON.parse(message.msg)
+    pc.setRemoteDescription(description).then().catch();
+    do_answer();
+}
+
+function handle_remote_answer(message) {
+    console.info("handle remote answer");
+    let description = JSON.parse(message.msg);
+    pc.setRemoteDescription(description).then().catch();
+}
+
+function handle_remote_candidate(message) {
+    console.info("handle remote candidate");
+    let candidate = JSON.parse(message.msg);
+    pc.addIceCandidate(candidate).then().catch(e => {
+        console.error("add ice candidate error" + e.name);
+    });
+}
+
+function do_offer() {
+    // 创建RTCPeerConnection
+    if (pc == null) {
+        create_peer_connection();
+    }
+    pc.createOffer().then(on_create_offer).catch(handle_create_offer_error);
+}
+
+function do_answer() {
+    pc.createAnswer().then(on_create_answer).catch(handle_create_answer_error);
+}
+
+function do_join(room_id) {
+    let json_msg = {
+        "cmd": SIGNAL_TYPE_JOIN,
+        "room_id": room_id,
+        "uid": local_user_id
+    };
+    let msg = JSON.stringify(json_msg);
+    rtc_engine.send_message(msg);
+    console.info("do join message: " + msg);
+}
+
+function do_leave(room_id) {
+    let json_msg = {
+        "cmd": SIGNAL_TYPE_LEAVE,
+        "room_id": room_id,
+        "uid": local_user_id
+    };
+    let msg = JSON.stringify(json_msg);
+    rtc_engine.send_message(msg);
+    console.info("do leave message: " + msg);
+}
+
+function open_local_stream(stream) {
+    console.log("open local stream");
+    do_join(room_id);
+    local_stream = stream;
+    local_video.srcObject = stream;
+}
+function init_local_stream() {
+    navigator.mediaDevices.getUserMedia({
+        video: true,
+        audio: true
+    }).then(open_local_stream).catch(function (e) {
+       alert("本地视频获取失败");
+       console.log("error" + e.name);
+    });
+}
+
+rtc_engine = new RTCEngine("ws://47.92.208.18:8010");
+rtc_engine.create_websocket();
+
+document.getElementById("join-btn").onclick = function () {
+    room_id = document.getElementById("room-id").value;
+    if (room_id === "" || room_id === "请输入房间ID") {
+        alert("请输入房间ID");
+        return;
+    }
+    console.log("加入房间" + room_id);
+    // 初始化本地码流
+    init_local_stream();
+};
+
+document.getElementById("leave-btn").onclick = function () {
+    console.log("离开房间");
+    do_leave(room_id);
+}
--- server/server.js
+++ server/server.js
@@ -1,32 +1,226 @@
+const MESSAGE_TYPE_OFFER = 0x01;
+const MESSAGE_TYPE_ANSWER = 0x02;
+const MESSAGE_TYPE_CANDIDATE = 0x03;
+const MESSAGE_TYPE_HANGUP = 0x04;
+// join 主动加入房间
+// leave 主动离开房间
+// new-peer 有人加入房间, 通知其他用户
+// peer-leave 有人离开房间, 通知其他用户
+// offer 发送offer给对端peer
+// answer 发送offer给对端peer
+// candidate 发送candidate给对端peer
+
+const SIGNAL_TYPE_JOIN = "join";
+const SIGNAL_TYPE_RESP_JOIN = "resp-join"; // 告知对方加入者是谁
+const SIGNAL_TYPE_LEAVE = "leave";
+const SIGNAL_TYPE_NEW_PEER = "new-peer";
+const SIGNAL_TYPE_PEER_LEAVE = "peer-leave";
+const SIGNAL_TYPE_OFFER = "offer";
+const SIGNAL_TYPE_ANSWER = "answer";
+const SIGNAL_TYPE_CANDIDATE = "candidate";
+
+let RTCMap = function () {
+   this._entrys = [];
+
+   this.put = function (key, value) {
+      if (key == null || key == undefined) {
+         return;
+      }
+      let index = this._get_index(key);
+      if (index === -1) {
+         let entry = {};
+         entry.key = key;
+         entry.value = value;
+         this._entrys[this._entrys.length] = entry;
+      }
+      else {
+         this._entrys[index].value = value;
+      }
+   };
+
+   this.get = function (key) {
+      let index = this._get_index(key);
+      return (index !== -1) ? this._entrys[index].value : null;
+   };
+
+   this.remove = function (key) {
+      let index = this._get_index(key);
+      if (index !== -1) {
+         this._entrys.splice(index, 1);
+      }
+   };
+
+   this.clear = function () {
+      this._entrys.length = 0;
+   };
+
+   this.contains = function (key) {
+      let index = this._get_index(key);
+      return index !== -1;
+   };
+
+   this.size = function () {
+      return this._entrys.length;
+   };
+
+   this.get_entrys = function () {
+      return this._entrys;
+   };
+
+   this._get_index = function (key) {
+      if (key == null || key == undefined) {
+         return -1;
+      }
+      let _length = this._entrys.length;
+      for (let i = 0; i < _length; ++i) {
+         let entry = this._entrys[i];
+         if (entry == null || entry == undefined) {
+            continue;
+         }
+         if (entry.key === key) {
+            return i;
+         }
+      }
+      return -1;
+   };
+}
+
+let Client = function (uid, connection, room_id) {
+   this.uid = uid;
+   this.connection = connection;
+   this.room_id = room_id;
+}
+
+let room_table_map = new RTCMap();
+
 let ws = require("nodejs-websocket");
 let port = 8010;
-let user = 0;
+
+function handle_join(connection, message) {
+   let room_id = message.room_id;
+   let uid = message.uid;
+   console.info("uid: " + uid + "join room: " + room_id);
+
+   let room_map = null;
+
+   if (!room_table_map.contains(room_id)) {
+      room_map = new RTCMap();
+      room_table_map.put(room_id, room_map);
+   }
+   else if (room_table_map.size() >= 2) {
+      console.error("房间" + room_id + "已满");
+      // TODO 报告客户端房间已满
+      return;
+   }
+   else {
+      room_map = room_table_map.get(room_id);
+   }
+
+   let client = new Client(uid, connection, room_id);
+   room_map.put(uid, client);
+
+   if (room_map.size() > 1) {
+      // 房间里已经有人了, 需要通知对方
+      let clients = room_map.get_entrys();
+      for (let i in clients) {
+         let remote_uid = clients[i].key;
+         if (remote_uid !== uid) {
+            let json_msg = {
+               "cmd": SIGNAL_TYPE_NEW_PEER,
+               "remote_uid": uid
+            };
+            let msg = JSON.stringify(json_msg);
+            let remote_client = room_map.get(remote_uid);
+            console.log("new-peer: " + msg);
+            remote_client.connection.sendText(msg);
+
+            json_msg = {
+               "cmd": SIGNAL_TYPE_RESP_JOIN,
+               "remote_uid": remote_uid
+            };
+            msg = JSON.stringify(json_msg);
+            console.log("resp-join: " + msg);
+            connection.sendText(msg);
+         }
+      }
+   }
+}
+
+function handle_leave(message) {
+   let room_id = message.room_id;
+   let uid = message.uid;
+
+   console.info("uid: " + uid + "leave room " + room_id);
+   if (!room_table_map.contains(room_id)) {
+      console.error("无法找到房间");
+      return;
+   }
+   let room_map = room_table_map.get(room_id);
+   room_map.remove(uid); // 删除发送者
+   // 将消息发送给其他人
+   if (room_map.size() > 0) {
+      let clients = room_map.get_entrys();
+      for (let i in clients) {
+         let json_msg = {
+            "cmd": SIGNAL_TYPE_PEER_LEAVE,
+            "remote_uid": uid
+         };
+         let msg = JSON.stringify(json_msg);
+         let remote_uid = clients[i].key;
+         let remote_client = room_map.get(remote_uid);
+         console.info("notify peer " + remote_uid + ", uid: " + uid + "leave");
+         remote_client.connection.sendText(msg);
+      }
+   }
+}
+
+function handle_offer(message) {
+   let room_id = message.room_id;
+   let uid = message.uid;
+   let remote_uid = message.remote_uid;
+
+   if (!room_table_map.contains(room_id)) {
+      console.error("handle offer can't find room " + room_id);
+      return;
+   }
+   let room_map = room_table_map.get(room_id);
+   if (!room_map.contains(uid)) {
+      console.error("handle offer can't find uid " + uid);
+      return;
+   }
+
+}
 
 // 创建一个连接
 let server = ws.createServer(function (connection) {
    console.log("创建一个新的连接----");
-   user++;
-   connection.nickname = "user" + user;
-   connection.fd = "user" + user;
-   let msg = {};
-   msg.type = "enter";
-   msg.data = connection.nickname + "进来啦";
-   broadcast(JSON.stringify(msg));
-
    // 向客户端推送消息
     connection.on("text", function (str) {
-       console.log("回复" + str);
-       msg.type = "message";
-       msg.data = connection.nickname + " 说: " + str;
-       broadcast(JSON.stringify(msg));
+       console.info("receive msg: " + str);
+       let json_msg = JSON.parse(str);
+
+       switch (json_msg.cmd) {
+          case SIGNAL_TYPE_JOIN:
+             handle_join(connection, json_msg);
+             break;
+          case SIGNAL_TYPE_LEAVE:
+             handle_leave(json_msg);
+             break;
+          case SIGNAL_TYPE_OFFER:
+             handle_offer(json_msg);
+             break;
+          case SIGNAL_TYPE_ANSWER:
+             handle_answer(json_msg);
+             break;
+          case SIGNAL_TYPE_CANDIDATE:
+             handle_candidate(json_msg);
+             break;
+       }
     });
 
     // 监听关闭连接的操作
    connection.on("close", function (code, reason) {
-      console.log("关闭连接");
-      msg.type = "leave";
-      msg.data = connection.nickname + "离开了";
-      broadcast(JSON.stringify(msg));
+      console.info("连接关闭 code: " +code + ", reason" +reason);
    });
 
    // 错误处理
@@ -35,10 +229,3 @@ let server = ws.createServer(function (connection) {
       console.log(error);
    });
 }).listen(port);
-
-// 广播函数
-function broadcast(str) {
-   server.connections.forEach(function (connection) {
-      connection.sendText(str);
-   })
-}
